8968d8dcdbdf6e14c2edb326157195f923e1519b
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2014-10-06 13:46:43 -0500

    Initial Commit

---
+++ Ejercicio1_Tarea4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ejercicio1_Tarea4
+//
+//  Created by Luis Alfonso Arriaga Quezada on 10/6/14.
+//  Copyright (c) 2014 Luis Alfonso Arriaga Quezada. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
