53306d018fe8f4251ad7ed8ebbc74a02abc95f93
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2014-10-06 23:02:10 -0500

    PRODUCTO FINAL (RESULTADO+LOOP)

--- Ejercicio1_Tarea4/Ejercicio1_Tarea4.cpp
+++ Ejercicio1_Tarea4/Ejercicio1_Tarea4.cpp
@@ -7,24 +7,33 @@
 //  un numero N de factores a sumar y el NUMERADOR y DENOMINADOR iniciales.
 //  Para efectos del calculo del siguiente NUMERADOR y DENOMINADOR se tomara lo siguiente:
 //  Si el DENOMINADOR actual es par, el siguiente se calcula como denominador + 3
-//  
+//  Si el denominador actual es impar el siguiente se calcula como denominador + 1.
+//
+// CASOS DE PRUEBA:
+// A) N = 7, NUM = 3, DEN = 5 = 3/5 + 4/6 + 5/9 + 6/10 + 7/13 + 8/14 + 9/17 = 4.06
+// B) N = 3, NUM = 8, DEN = 3, = 8/3 + 9/4 + 10/7 = 6.34
+
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     
     int iTerminos, iNum, iDen, iTest = 1;
     double dTotal=0.0,dNum,dDen;
+    char CReset;
     
-    cout << "Ingresa el numero de terminos de la sumatoria" << endl;
-    cin >> iTerminos;
-    cout << "ingresa el numerador inicial" << endl;
-    cin >> iNum;
-    cout<< "Ingresa el Denominador inicial" << endl;
-    cin>> iDen;
-    
-    for (int iContador=2; iContador<=iTerminos; iContador=iContador+1) {
+    do
+    {
+        cout << "Ingresa el numero de terminos de la sumatoria" << endl;
+        cin >> iTerminos;
+        cout << "ingresa el numerador inicial" << endl;
+        cin >> iNum;
+        cout<< "Ingresa el Denominador inicial" << endl;
+        cin>> iDen;
+   
+    for (int iContador=2; iContador<=iTerminos; iContador++) {
         
         
         if (iTest == 1){
@@ -52,10 +61,18 @@ int main() {
         }
         
         if (iContador == iTerminos)
-            cout << iNum << '/' << iDen << " = " << dTotal << endl;
+            cout << iNum << '/' << iDen << " = "<< setprecision(3) << dTotal << endl;
         else
             cout << iNum << '/' << iDen << " + ";
     }
+        cout << "Desea hacer otra sumatoria (y / n)"<< endl;
+        cin >> CReset;
+        CReset = tolower(CReset);
+    
+    } while (CReset == 'y');
+    
     
     return 0;
 }
+
+
