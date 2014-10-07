5e3f81aeed58af6e28fc206c0bd914b7e1a4cfa3
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2014-10-06 22:06:20 -0500

    Solucion Final

--- Ejercicio1_Tarea4/Ejercicio1_Tarea4.cpp
+++ Ejercicio1_Tarea4/Ejercicio1_Tarea4.cpp
@@ -1,10 +1,13 @@
-//
-//  main.cpp
 //  Ejercicio1_Tarea4
 //
 //  Created by Luis Alfonso Arriaga Quezada on 10/6/14.
 //  Copyright (c) 2014 Luis Alfonso Arriaga Quezada. All rights reserved.
 //
+//  Este programa resuelve una sumatoria de fracciones dadas, para esto el usuario ingresa
+//  un numero N de factores a sumar y el NUMERADOR y DENOMINADOR iniciales.
+//  Para efectos del calculo del siguiente NUMERADOR y DENOMINADOR se tomara lo siguiente:
+//  Si el DENOMINADOR actual es par, el siguiente se calcula como denominador + 3
+//  
 
 #include <iostream>
 using namespace std;
@@ -12,7 +15,7 @@ using namespace std;
 int main() {
     
     int iTerminos, iNum, iDen, iTest = 1;
-    double iTotal;
+    double dTotal=0.0,dNum,dDen;
     
     cout << "Ingresa el numero de terminos de la sumatoria" << endl;
     cin >> iTerminos;
@@ -26,23 +29,32 @@ int main() {
         
         if (iTest == 1){
             cout << iNum << '/' <<iDen << " + ";
+            dNum = iNum*1.0;
+            dDen = iDen*1.0;
+            dTotal = dNum/dDen;
             iTest++;
         }
         
         if (iDen % 2 == 0) {
             iDen=iDen+3;
             iNum++;
-            cout << iNum << '/' << iDen << " + ";
+            dNum = iNum*1.0;
+            dDen = iDen*1.0;
+            dTotal = dTotal + (dNum/dDen);
         }
         
         else if(iDen % 2 != 0) {
             iDen=iDen+1;
             iNum++;
-            cout << iNum << '/' << iDen << " + ";
-        
+            dNum = iNum*1.0;
+            dDen = iDen*1.0;
+            dTotal = dTotal + (dNum/dDen);
         }
         
-        
+        if (iContador == iTerminos)
+            cout << iNum << '/' << iDen << " = " << dTotal << endl;
+        else
+            cout << iNum << '/' << iDen << " + ";
     }
     
     return 0;
